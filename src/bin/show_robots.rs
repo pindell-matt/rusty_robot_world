8079198018e9c8db78255fb882eae05ad71259f8
pindell-matt <pindell.m@protonmail.com>
2018-01-17 20:23:40 -0500

    comment out as unneeded

--- src/bin/show_robots.rs
+++ src/bin/show_robots.rs
@@ -1,23 +1,23 @@
-extern crate diesel_demo;
-extern crate diesel;
+// extern crate diesel_demo;
+// extern crate diesel;
 
-use self::diesel_demo::*;
-use self::models::*;
-use self::diesel::prelude::*;
+// use self::diesel_demo::*;
+// use self::models::*;
+// use self::diesel::prelude::*;
 
-fn main() {
-    use diesel_demo::schema::robots::dsl::*;
+// fn main() {
+//     use diesel_demo::schema::robots::dsl::*;
 
-    let connection = establish_connection();
-    let results = robots.filter(published.eq(true))
-        .limit(5)
-        .load::<Robot>(&connection)
-        .expect("Error loading robots");
+//     let connection = establish_connection();
+//     let results = robots.filter(published.eq(true))
+//         .limit(5)
+//         .load::<Robot>(&connection)
+//         .expect("Error loading robots");
 
-    println!("Displaying {} robots", results.len());
-    for post in results {
-        println!("{}", robot.name);
-        println!("----------\n");
-        println!("{}", robot.avatar);
-    }
-}
+//     println!("Displaying {} robots", results.len());
+//     for post in results {
+//         println!("{}", robot.name);
+//         println!("----------\n");
+//         println!("{}", robot.avatar);
+//     }
+// }
